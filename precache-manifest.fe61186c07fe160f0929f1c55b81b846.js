eaccd5630b9d7ae808022a098d2636f5300abbc5
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-09-13 20:59:29 -0700

    first heroku commit

---
+++ precache-manifest.fe61186c07fe160f0929f1c55b81b846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38aaec6095cf0f2698725b3a636f5080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45e25b80b3fd6a384c12",
+    "url": "/static/css/main.fe06f4a0.chunk.css"
+  },
+  {
+    "revision": "ec6273fc4a70790559c6",
+    "url": "/static/js/2.bb7446c2.chunk.js"
+  },
+  {
+    "revision": "45e25b80b3fd6a384c12",
+    "url": "/static/js/main.8351fcca.chunk.js"
+  },
+  {
+    "revision": "6ba9c9b4022516f0cfdf",
+    "url": "/static/js/runtime~main.386ea463.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/static/media/click2.fe0ded65.wav"
+  }
+]);
